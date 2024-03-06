e459527fec107eafed65967eaa51f327e7b54d58
adasilvamart <adasilva.clase@gmail.com>
2024-03-06 17:55:48 +0100

    feat(item): :construction: Create Ask Class

---
+++ app/src/main/java/edu/badpals/stockx/item/Ask.java
@@ -0,0 +1,29 @@
+package edu.badpals.stockx.item;
+
+public class Ask implements Offer {
+    private String size = "";
+    private Integer ask = 0;
+
+    public Ask(String size, int ask) {
+        this.size = size;
+        this.ask = ask;
+    }
+
+    public String size() {
+        return this.size;
+    }
+    
+    public int value() {
+        return this.ask.intValue();
+    }
+
+    public int compareTo(Offer offer) {
+        return this.ask.compareTo(offer.value());
+    }
+
+    @Override
+    public String toString() {
+        return "\t\t" + this.size() + "\t" + this.value() +"\n"; 
+    }
+
+}
