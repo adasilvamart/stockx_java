db4a09e581576b987e8785eb002ef29f5b7726ba
adasilvamart <adasilva.clase@gmail.com>
2024-03-06 17:38:59 +0100

    test: :white_check_mark: Create Bid.compareTo Test

---
+++ app/src/test/java/edu/badpals/stockx/item/BidTest.java
@@ -0,0 +1,34 @@
+package edu.badpals.stockx.item;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+    
+    
+public class BidTest {
+
+    @Test
+    public void testBidCompareTo() {
+        Bid firstBid = new Bid("9.5", 120);
+        Bid secondBid = new Bid("9.5", 120);
+        //Bid thirdBid = new Bid("9.5", 130);
+        int expResult = 0;
+        assertEquals(firstBid.compareTo(secondBid), expResult);
+    }
+
+    @Test
+    public void testBidCompareToLess() {
+        Bid firstBid = new Bid("9.5", 120);
+        Bid secondBid = new Bid("9.5", 130);
+        int expResult = -1;
+        assertEquals(firstBid.compareTo(secondBid), expResult);
+    }
+
+    @Test
+    public void testBidCompareToMore() {
+        Bid firstBid = new Bid("9.5", 120);
+        Bid secondBid = new Bid("9.5", 130);
+        int expResult = 1;
+        assertEquals(firstBid.compareTo(secondBid), expResult);
+    }
+}
+    
