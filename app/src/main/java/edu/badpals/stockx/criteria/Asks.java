e8589c00bffe546e08591ede26056f7def9edeef
adasilvamart <adasilva.clase@gmail.com>
2024-03-07 12:50:06 +0100

    feat(criteria): :construction: Create Asks Class

---
+++ app/src/main/java/edu/badpals/stockx/criteria/Asks.java
@@ -0,0 +1,19 @@
+package edu.badpals.stockx.criteria;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import edu.badpals.stockx.item.*;
+
+public class Asks implements Criteria {
+    
+    public Asks() {} 
+
+    @Override
+    public List<Offer> checkCriteria(Item item) {
+        return item.offers().stream()
+                            .filter(Ask.class::isInstance)
+                            .collect(Collectors.toList());
+    }
+}
