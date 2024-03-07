98d0f6dbf870dadf226f3a05915c8092e82a40bc
adasilvamart <adasilva.clase@gmail.com>
2024-03-07 17:40:48 +0100

    refactor(criteria): :recycle: Refactor checkCriteria
    Using Bids.checkCriteria instead of filtering

--- app/src/main/java/edu/badpals/stockx/criteria/MaxBid.java
+++ app/src/main/java/edu/badpals/stockx/criteria/MaxBid.java
@@ -3,20 +3,19 @@ package edu.badpals.stockx.criteria;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-
-import edu.badpals.stockx.item.Bid;
 import edu.badpals.stockx.item.Item;
 import edu.badpals.stockx.item.Offer;
 
 public class MaxBid implements Criteria {
 
-    public MaxBid(){}
+    public MaxBid() {
+    }
 
     public List<Offer> checkCriteria(Item item) {
-        Optional<Offer> maxBid = item.offers().stream()
-                            .filter(Bid.class::isInstance)
-                            .max(Comparator.naturalOrder());
-        
+        Bids bidsFilter = new Bids();
+        Optional<Offer> maxBid = bidsFilter.checkCriteria(item).stream()
+                .max(Comparator.naturalOrder());
+
         if (maxBid.isPresent()) {
             return List.of(maxBid.get());
         }
