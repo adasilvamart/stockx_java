d1a3540be21fe44f507a0b869bf37308c10aed85
adasilvamart <adasilva.clase@gmail.com>
2024-03-07 12:02:51 +0100

    feat(item): :sparkles: Extends from Comparable

--- app/src/main/java/edu/badpals/stockx/item/Offer.java
+++ app/src/main/java/edu/badpals/stockx/item/Offer.java
@@ -1,6 +1,6 @@
 package edu.badpals.stockx.item;
 
-public interface Offer {
+public interface Offer extends Comparable<Offer> {
     public String size();
     public int value();
     public int compareTo(Offer offer);
