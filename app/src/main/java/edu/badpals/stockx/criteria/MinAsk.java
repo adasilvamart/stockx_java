083a740589c35a43bfe653db11c39c439e54b6e0
adasilvamart <adasilva.clase@gmail.com>
2024-03-07 17:39:48 +0100

    feat(criteria): :construction: Create MinAsk Class

---
+++ app/src/main/java/edu/badpals/stockx/criteria/MinAsk.java
@@ -0,0 +1,30 @@
+package edu.badpals.stockx.criteria;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
+import edu.badpals.stockx.item.Item;
+import edu.badpals.stockx.item.Offer;
+
+public class MinAsk implements Criteria {
+
+    public MinAsk() {
+    }
+
+    @Override
+    public List<Offer> checkCriteria(Item item) {
+
+        Asks asksFilter = new Asks();
+        Optional<Offer> minAsk = asksFilter.checkCriteria(item)
+                .stream()
+                .min(Comparator.naturalOrder());
+
+        if (minAsk.isPresent()) {
+            return List.of(minAsk.get());
+        }
+
+        return List.of();
+
+    }
+}
