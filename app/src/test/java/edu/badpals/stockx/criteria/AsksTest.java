64cff6554101a4a1cbfb0b268cd739e79dcfa334
adasilvamart <adasilva.clase@gmail.com>
2024-03-07 12:55:56 +0100

    test(test): :white_check_mark: Refactor Asks and Bids tests

--- app/src/test/java/edu/badpals/stockx/criteria/AsksTest.java
+++ app/src/test/java/edu/badpals/stockx/criteria/AsksTest.java
@@ -1,31 +1,31 @@
 package edu.badpals.stockx.criteria;
 import org.junit.Test;
 
-import edu.badpals.stockx.item.Bid;
+import edu.badpals.stockx.item.Ask;
+import edu.badpals.stockx.criteria.Asks;
 import edu.badpals.stockx.item.Item;
 import edu.badpals.stockx.item.Offer;
 import edu.badpals.stockx.item.Sneaker;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.List;
     
     
-public class BidsTest {
+public class AsksTest {
 
     @Test
     public void testBidsCheckCriteria() {
 
         Item sneaker = new Sneaker("555088-105", "Jordan 1 Retro High Dark Mocha");
-        sneaker.add(new Bid("13", 550));
-        sneaker.add(new Bid("6", 200));
+        sneaker.add(new Ask("13", 550));
+        sneaker.add(new Ask("6", 200));
 
-        Bids bidsFilter = new Bids();
+        Asks asksFilter = new Asks();
         int expResult = 2;
-        assertEquals(bidsFilter.checkCriteria(sneaker).stream().count(), expResult);
+        assertEquals(asksFilter.checkCriteria(sneaker).stream().count(), expResult);
     }
+
 }
     
